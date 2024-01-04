581f3a69154803e994d4b375c19786a89d8d738a
khankomail <cobraking1.ck@gmail.com>
2024-01-04 13:22:38 +0500

    added cart

--- chainocity/urls.py
+++ chainocity/urls.py
@@ -21,6 +21,11 @@ from django.conf import settings
 
 from django.conf.urls.static import static
 
+
+admin.site.site_header = "Chainocity Admin"
+admin.site.site_title = "Chainocity Admin Portal"
+admin.site.index_title = "Welcome to Chainocity Admin Portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls'))
